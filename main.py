cff4e1e0fb25fdd6139d83bf5ac965e5b7709404
bane80ns <bane80@gmail.com>
2025-02-03 20:23:24 +0100

    Merged 1 day and 7 days functions into one, and now we need to provide number of days into function, and as result we return dictionary with (user_id: activity_count) for 10 highest active users

--- main.py
+++ main.py
@@ -5,18 +5,14 @@ from datetime import datetime
 db_name = "activity_db"
 
 connection = pymysql.connect(
-    host='localhost',  # localhost - Local PC, or 127.0.0.1 (This field is for DB IP address)
-    user='root',  # username created for accessing DB (Database)
-    password='root',  # password created for accessing DB
+    host="localhost",  # localhost - Local PC, or 127.0.0.1 (This field is for DB IP address)
+    user="root",  # username created for accessing DB (Database)
+    password="root",  # password created for accessing DB
     database=db_name,  # DB name
-    cursorclass=pymysql.cursors.DictCursor
+    cursorclass=pymysql.cursors.DictCursor,
     #   port=xxxx # Port which we use for connecting to DB (in our case not needed)
-    )
+)
 cursor = connection.cursor()
-
-
-
-# print(datetime.now())
 
 
 # def to check if activity is valid
@@ -31,66 +27,41 @@ def activity_type_check(activity):
         raise ValueError("Activity type is not valid")
 
 
-# add activity into db with
+# Add activity into db (activity_table) with activity type taken from activity_type dict, also adds time when that activity happend.
 def add_activity(activity, user_id):
     activity_type = activity_type_check(activity)
-    query_date = ("INSERT INTO activity_table (user_id, activity_name, entrance_datetime) VALUES (%s, %s, %s) ")
+    query_date = "INSERT INTO activity_table (user_id, activity_name, entrance_datetime) VALUES (%s, %s, %s) "
     sql_values = (user_id, activity, datetime.now())
     cursor.execute(query_date, sql_values)
     connection.commit()
     cursor.close()
     connection.close()
 
-
-
 # add_activity("homework", 18)
 
 
-
-
-# Check highest activity in 7 days, and list 10 highest active users descending
-def last_week_activity():
-
-    query_data = ("SELECT user_id, COUNT(*) AS activity_count "
-                  "FROM activity_table "
-                  "WHERE NOT activity_name LIKE '%log%' "
-                  "AND entrance_datetime >= NOW() - INTERVAL 7 DAY "
-                  "GROUP BY user_id "
-                  "ORDER BY activity_count DESC "
-                  "LIMIT 10")
-
-    cursor.execute(query_data)
-    rows = cursor.fetchall()
-    for row in rows:
-        print(f"User id:{row["user_id"]:4}       Activity Count: {row["activity_count"]:2}")
-    cursor.close()
-    connection.close()
-# last_week_activity()
-
-
-# Check current day activities, and list active users descending by activity
-def current_day_activity():
-    query_data = ("SELECT user_id, COUNT(*) AS activity_count "
-                  "FROM activity_table "
-                  "WHERE entrance_datetime >= NOW() - INTERVAL 1 DAY "
-                  "GROUP BY user_id "
-                  "ORDER BY activity_count DESC ")
+# Read base (activity table) for highly active users (10 of them) in X days (we provide number of days into function) and as return we get dict with {[user_id, activity_count]} descending.
+def daily_activities(number_of_days):
+    query_data = f"""
+        SELECT user_id, COUNT(*) AS activity_count
+        FROM activity_table
+        WHERE NOT activity_name LIKE '%log%'
+        AND entrance_datetime >= NOW() - INTERVAL {int(number_of_days)} DAY
+        GROUP BY user_id
+        ORDER BY activity_count DESC
+        LIMIT 10
+    """
 
     cursor.execute(query_data)
     rows = cursor.fetchall()
+
+    top_ten = {}
     for row in rows:
-        print(f"User id:{row["user_id"]:4}       Activity Count: {row["activity_count"]:2}")
+        top_ten[row["user_id"]] = row["activity_count"]
+
     cursor.close()
     connection.close()
+    return top_ten
 
 
-# current_day_activity()
-
-
-
-
-
-
-
-
-
+print(daily_activities(7))
